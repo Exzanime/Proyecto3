91779f85ec2210610beb7a1c1837b360ee281862
nachoMillan8 <nachomas1@hotmail.es>
2024-12-10 21:28:30 +0100

    Update EventoServiceImplTest.java

---
+++ eventoService/src/test/java/com/example/eventoService/EventoServiceImplTest.java
@@ -0,0 +1,107 @@
+package com.example.eventoService;
+
+import com.example.eventoService.controller.error.EventoDuplicateExcp;
+import com.example.eventoService.dto.DtoEvento;
+import com.example.eventoService.entity.Evento;
+import com.example.eventoService.repository.EventoRepository;
+import com.example.eventoService.service.EventoServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+@SpringBootTest
+public class EventoServiceImplTest {
+    @Mock
+    EventoRepository eventoRepository;
+    @InjectMocks
+    EventoServiceImpl eventoService;
+
+    private DtoEvento dtoEvento;
+
+    private Evento eventoExistente;
+
+    @BeforeEach
+    public void setUp() {
+        openMocks(this);
+
+        dtoEvento = DtoEvento.builder()
+                .nombre("Concierto Metal")
+                .genero("Metal")
+                .localidad("Madrid")
+                .recinto("Palacio de Deportes")
+                .descripcion("descripcion")
+                .fecha(LocalDateTime.now().plusDays(10))
+                .precioMin(30.0)
+                .precioMax(100.0)
+                .build();
+
+        eventoExistente = Evento.builder()
+                .nombre("Concierto Metal")
+                .genero("Metal")
+                .localidad("Madrid")
+                .recinto("Palacio de Deportes")
+                .descripcion("descripcion")
+                .fecha(dtoEvento.getFecha())
+                .precioMin(30.0)
+                .precioMax(100.0)
+                .build();
+    }
+    @Test
+    public void testGetDetallesEvento() {
+        when(eventoRepository.findById(1L)).thenReturn(null);
+        eventoService.getDetalleEvento(1L);
+        assert(eventoService.getDetalleEvento(1L)==null);
+    }
+
+    @Test
+    public void testSaveEventoCorrectamente() {
+        Evento evento = Evento.builder()
+                .nombre("Concierto Metal")
+                .genero("Metal")
+                .localidad("Madrid")
+                .recinto("Palacio de Deportes")
+                .descripcion("descripcion")
+                .fecha(LocalDateTime.now().plusDays(10))
+                .precioMin(30.0)
+                .precioMax(100.0)
+                .build();
+
+        when(eventoRepository.save(any(Evento.class))).thenReturn(evento);
+
+        DtoEvento result = eventoService.saveEvento(dtoEvento);
+
+        verify(eventoRepository, times(1)).save(any(Evento.class));
+        assert(result.getNombre().equals(evento.getNombre()));
+        assert(result.getGenero().equals(evento.getGenero()));
+        assert(result.getLocalidad().equals(evento.getLocalidad()));
+        assert(result.getRecinto().equals(evento.getRecinto()));
+        assert(result.getFecha().equals(evento.getFecha()));
+        assert(result.getPrecioMin() == evento.getPrecioMin());
+        assert(result.getPrecioMax() == evento.getPrecioMax());
+    }
+
+    @Test
+    public void testSaveEventoLanzaExcepcionSiExiste() {
+        // Simular que ya existe un evento con el mismo nombre, género y fecha
+        when(eventoRepository.findByNombreAndGeneroAndFecha(
+                dtoEvento.getNombre(),
+                dtoEvento.getGenero(),
+                dtoEvento.getFecha()))
+                .thenReturn(Optional.of(eventoExistente));
+
+        assertThrows(EventoDuplicateExcp.class, () -> eventoService.saveEvento(dtoEvento));
+
+        verify(eventoRepository, times(1)).findByNombreAndGeneroAndFecha(dtoEvento.getNombre(), dtoEvento.getGenero(), dtoEvento.getFecha());
+
+        verify(eventoRepository, never()).save(any(Evento.class));
+    }
+}

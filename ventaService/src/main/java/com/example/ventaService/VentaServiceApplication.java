5584391f5953fdaa8b9dcee9e9688e791ae7e3b2
nachoMillan8 <nachomas1@hotmail.es>
2024-12-14 13:07:18 +0100

    v

--- ventaService/src/main/java/com/example/ventaService/VentaServiceApplication.java
+++ ventaService/src/main/java/com/example/ventaService/VentaServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.ventaService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VentaServiceApplication {
 
 	public static void main(String[] args) {

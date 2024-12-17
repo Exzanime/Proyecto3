f4bb300edff9ac9b07d0598dec6a4d00fdf6dac6
Alejandro <alex02087@hotmail.com>
2024-12-17 09:42:38 +0100

    validarUsuario

--- ventaService/src/main/java/com/example/ventaService/controller/ValidarController.java
+++ ventaService/src/main/java/com/example/ventaService/controller/ValidarController.java
@@ -2,8 +2,12 @@ package com.example.ventaService.controller;
 
 import com.example.ventaService.dtos.UserValidationRequest;
 import com.example.ventaService.dtos.UserValidationResponse;
+import com.example.ventaService.dtos.VentaValidationRequest;
+import com.example.ventaService.dtos.VentaValidationResponse;
 import com.example.ventaService.feignClient.BancoClient;
 import feign.FeignException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class ValidarController {
 
     private final BancoClient bancoClient;
+    private String token;
 
     public ValidarController(BancoClient bancoClient) {
         this.bancoClient = bancoClient;
@@ -24,6 +29,15 @@ public class ValidarController {
             return ResponseEntity.ok(response);
         } catch (FeignException e) {
             return ResponseEntity.status(e.status()).body("Error: " + e.getMessage());
+        }
+    }
+    @PostMapping("/venta")
+    public ResponseEntity<?> validarVenta(@RequestBody VentaValidationRequest request,@RequestHeader("Authorization") String jwtToken) {
+        try {
+            VentaValidationResponse response = bancoClient.validarVenta(request, jwtToken);
+            return ResponseEntity.ok(response);
+        } catch (FeignException e) {
+            return ResponseEntity.internalServerError().body("Error: " + e.getMessage());
         }
     }
 }

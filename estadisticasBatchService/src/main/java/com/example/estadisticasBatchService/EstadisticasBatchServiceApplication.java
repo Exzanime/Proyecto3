125aa2ffe5993216661983180f488f632c1c0606
Leto <rustedcity@gmail.com>
2024-12-18 15:59:53 +0100

    creación módulo

---
+++ estadisticasBatchService/src/main/java/com/example/estadisticasBatchService/EstadisticasBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.estadisticasBatchService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadisticasBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadisticasBatchServiceApplication.class, args);
+	}
+
+}

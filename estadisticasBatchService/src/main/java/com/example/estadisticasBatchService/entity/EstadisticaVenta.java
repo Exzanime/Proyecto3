8cbfadba3d3c717d788e49e232f0db74754284ce
Leto <rustedcity@gmail.com>
2024-12-19 14:13:17 +0100

    ayuda

--- estadisticasBatchService/src/main/java/com/example/estadisticasBatchService/entity/EstadisticaVenta.java
+++ estadisticasBatchService/src/main/java/com/example/estadisticasBatchService/entity/EstadisticaVenta.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Entity
-@Data
+//@Entity
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class EstadisticaVenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +22,36 @@ public class EstadisticaVenta {
     private LocalDate fecha;
     private String nombreEvento;
     private double mediaPrecio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombreEvento() {
+        return nombreEvento;
+    }
+
+    public void setNombreEvento(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+    }
+
+    public double getMediaPrecio() {
+        return mediaPrecio;
+    }
+
+    public void setMediaPrecio(double mediaPrecio) {
+        this.mediaPrecio = mediaPrecio;
+    }
 }
